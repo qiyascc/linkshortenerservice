e849662b86770efcad3e6c2bc483ad0c82e52052
Qiyas <107604130+qiyascc@users.noreply.github.com>
2024-12-18 11:13:56 +0400

    Update settings.py

--- linkshortenerservice/linkshortenerservice/settings.py
+++ linkshortenerservice/linkshortenerservice/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r3&8vi3f=)ex-dkp05xjbk01dwh&u^p0@w)qgj57)#db%7%jpr'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
